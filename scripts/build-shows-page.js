907b42d2722e728297b50a5956ee01dc21eb4e8f
Alec Adolph <alecdadolph@gmail.com>
2022-12-12 16:51:12 -0700

    Added shows section through Java DOM and updated styling

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,52 +1,86 @@
 const shows = [
-    {
-    "date": "Mon Sept 06 2021",
-    "venue": "Ronald Lane",
-    "location": "San Francisco, CA"
-    },
-    {
-    "date": "Tue Sept 21 2021",
-    "venue": "Pier 3 East",
-    "location": "San Francisco, CA"   
-    },
-    {
-    "date": "Fri Oct 15 2021",
-    "venue": "View Lounge",
-    "location": "San Francisco, CA"   
-    },
-    {
-    "date": "Sat Nov 06 2021",
-    "venue": "Hyatt Agency",
-    "location": "San Francisco, CA"   
-    },
-    {
-    "date": "Fri Nov 26 2021",
-    "venue": "Moscow Center",
-    "location": "San Francisco, CA"   
-    },
-    {
-    "date": "Wed Dec 15 2021 ",
-    "venue": "Press Club",
-    "location": "San Francisco, CA"   
-    }
-]
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tue Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Oct 15 2021",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Nov 06 2021",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Wed Dec 15 2021 ",
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
+];
 
-    
+const h3 = (title, container) => {
+  const titleEl = document.createElement("h3");
+  titleEl.innerText = title;
+  titleEl.classList.add("shows__title");
+  container.append(titleEl);
+};
 
-    for (let i = 0; i < shows.length; ++i) {
-        const showsItem = shows[i].date;
-        //creating var to hold string
+const showsSection = document.querySelector(".showsContainer");
 
-        // const venueItem = shows[i].venue;
-        // const locationItem = shows[i].location;
+const showsTitle = document.createElement("h2");
+showsTitle.innerText = "Shows";
+showsSection.append(showsTitle);
 
-        let container = document.createElement("div");
-        //creating the div container
-        let element = document.createElement("p");
-        //creating p element to hold text from object
-        element.innertext = showsItem.date;
-        element.classList.add("shows__title")
-        //adding class to p element
+const displayShows = (show) => {
+  const showContainerEl = document.createElement("article");
+  showContainerEl.classList.add("shows");
 
-        container.append(element);
-    }
+  h3("DATE", showContainerEl);
+
+  const dateEl = document.createElement("p");
+  dateEl.innerText = show.date;
+  dateEl.classList.add("shows__details");
+  showContainerEl.append(dateEl);
+
+  h3("VENUE", showContainerEl);
+
+  const venueEl = document.createElement("p");
+  venueEl.innerText = show.venue;
+  venueEl.classList.add("shows__details");
+  showContainerEl.append(venueEl);
+
+  h3("LOCATION", showContainerEl);
+
+  const locationEl = document.createElement("p");
+  locationEl.innerText = show.location;
+  locationEl.classList.add("shows__details");
+  showContainerEl.append(locationEl);
+
+  const buttonEl = document.createElement("button");
+  buttonEl.innerText = "BUY TICKETS";
+  buttonEl.classList.add("shows__button");
+  showContainerEl.append(buttonEl);
+
+  showsEl.append(showContainerEl);
+};
+
+const showsEl = document.querySelector(".showsContainer");
+
+for (let i = 0; i < shows.length; i++) {
+  const showsItem = shows[i];
+
+  displayShows(showsItem);
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,17 +1,76 @@
 const comments = [
-    {
-    "name": "Connor Walton",
-    "date": "02/17/2021",
-    "type": "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-    },
-    {
-    "name": "Emilie Beach",
-    "date": "01/09/2021",
-    "type": "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-    },
-    {
-    "name": "Miles Acosta",
-    "date": "12/20/2020",
-    "type": "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough."
-    }
-]
+  {
+    name: "Connor Walton",
+    date: "02/17/2021",
+    type: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Emilie Beach",
+    date: "01/09/2021",
+    type: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Miles Acosta",
+    date: "12/20/2020",
+    type: "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
+  },
+];
+
+// Create a function "displayComments"
+
+// Loop over the comments
+
+// Build the comment <article>
+
+// Create a wrapper div for the img
+
+// Create a wrapper div for the comment info
+
+// Create a wrapper for the name / date
+
+// Put the comment text
+
+// Invoke the displayComments()
+
+// ---
+
+// Select the form element via JS
+
+// Select the comments section via JS
+
+// Add an event listener
+
+// prevent default (don't refresh the page)
+
+// Get the name
+// const name = event.target.name.value
+
+// Get the comment
+
+// Validate that the user provided a name + comment
+// if(!name || !comment) {
+
+// If name was empty
+// Get the name input element
+// Add a class to it (form__input--error)
+
+// If comment was empty
+// Get the comment input element
+// Add a class to it (form__input--error)
+
+// return;
+// }
+
+// Get rid of the error class on the inputs
+
+// Genreate the date for today "21/11/2022"
+// const date = new Date().toLocaleDateString()
+
+// add (comments.push()) a new object to the comment array
+
+// comments.push({name: name, comment: comment, date: date })
+
+// Clear the existing comments on the page
+// commentsSection.innerHTML = "";
+
+// Run the function above (run the loop)
