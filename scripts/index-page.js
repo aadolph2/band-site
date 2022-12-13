1132ed80771c9c6a63f522ebb2261ee1726e5fe9
Alec Adolph <alecdadolph@gmail.com>
2022-12-12 17:54:44 -0700

    index.js added foundation of function to display comments section and corresponding classes in sass

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,31 +2,51 @@ const comments = [
   {
     name: "Connor Walton",
     date: "02/17/2021",
-    type: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
   {
     name: "Emilie Beach",
     date: "01/09/2021",
-    type: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
     name: "Miles Acosta",
     date: "12/20/2020",
-    type: "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
+    comment:
+      "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
   },
 ];
 
-// Create a function "displayComments"
+const displayComments = (commentsArray) => {
+  for (let i = 0; i < commentsArray.length; i++) {
+    const commentsData = commentsArray[i];
+    const commentEl = document.createElement("article");
+    const imgEl = document.createElement("div");
+    const nameDateEl = document.createElement("div");
+    const contentEl = document.createElement("div");
+    const commentInfoEl = document.createElement("p");
+    const nameEl = document.createElement("p");
+    const dateEl = document.createElement("p");
 
-// Loop over the comments
+    nameEl.innerText = commentsData.name;
+    dateEl.innerText = commentsData.date;
+    commentInfoEl.innerText = commentsData.comment;
+  }
+};
+const commentsContainer = document.querySelector(".commentsContainer");
 
-// Build the comment <article>
+displayComments(comments);
 
-// Create a wrapper div for the img
-
-// Create a wrapper div for the comment info
-
-// Create a wrapper for the name / date
+//commentsEl wraps everything, name and date wrapped by nameDateEl,
+//flex-direction column nameDateCommentWrapper
+//article
+///imgDiv
+////contentDiv
+/////nameDateWrapper
+/////----name+date
+/////commentinfo
 
 // Put the comment text
 
